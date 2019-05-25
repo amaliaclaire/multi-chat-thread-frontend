aa5d69f0e0223c02597f477ab092beb05451ef76
Amalia <amalia.fergin@gmail.com>
2019-05-25 15:28:50 -0700

    getting started has rendered

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Container } from 'semantic-ui-react';
+import React, {Component} from "react";
 
-// import Login from './Login';
-// import JobBoard from './JobBoard';
-// import JobBoard from './components/JobBoard';
-// import Ranking from './components/Ranking'
-// import Comments from './components/Comments'
+import { Container } from 'semantic-ui-react';
+import './App.css'
+import axios from 'axios'
+import Comments from './components/Comments'
 import ContainerExample from './components/ContainerExample'
 
+const baseURL = 'http://localhost:3000/tickets'
+
+class App extends Component {
+  state = { isShown: false, request: {}}
+
+  componentDidMount() {
+
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <h1>Getting Started</h1>
+      </div>
+    )
+  }
+}
 
 
 
-const App = () => (
-  <Container>
-    <ContainerExample />
-
-  </Container>
-);
 
 export default App;
